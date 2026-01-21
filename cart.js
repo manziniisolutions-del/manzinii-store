2aa028a09b2faf794c544f0d59387f502788f647
manziniisolutions-del <manziniisolutions@gmail.com>
2026-01-21 18:31:59 +0200

    Delete cart.js

--- cart.js
+++
@@ -1,6 +0,0 @@
-function addToCart(item) {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push(item);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart");
-}
